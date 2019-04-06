3413344d5e9821b37cc42b81c488d51c907e0027
toworksyn <39189781+toworksyn@users.noreply.github.com>
2019-04-06 18:16:18 +0800

    Update priority_queue.c

--- src_code/priority_queue.c
+++ src_code/priority_queue.c
@@ -52,64 +52,64 @@ int sy_pq_init(sy_pq_t *sy_pq, sy_pq_comparator_pt comp, size_t size){
     return 0;
 }
 
-int tk_pq_is_empty(tk_pq_t *tk_pq){
+int sy_pq_is_empty(sy_pq_t *sy_pq){
     // 通过nalloc值款快速判断是否为空
-    return (tk_pq->nalloc == 0) ? 1 : 0;
+    return (sy_pq->nalloc == 0) ? 1 : 0;
 }
 
-size_t tk_pq_size(tk_pq_t *tk_pq){
+size_t sy_pq_size(sy_pq_t *sy_pq){
     // 获取优先队列大小
-    return tk_pq->nalloc;
+    return sy_pq->nalloc;
 }
 
-void *tk_pq_min(tk_pq_t *tk_pq){
+void *sy_pq_min(sy_pq_t *sy_pq){
     // 优先队列最小值直接返回第一个元素（指针）
-    if (tk_pq_is_empty(tk_pq))
+    if (sy_pq_is_empty(sy_pq))
         return (void *)(-1);
 
-    return tk_pq->pq[1];
+    return sy_pq->pq[1];
 }
 
 
-int resize(tk_pq_t *tk_pq, size_t new_size){
-    if(new_size <= tk_pq->nalloc)
+int resize(sy_pq_t *sy_pq, size_t new_size){
+    if(new_size <= sy_pq->nalloc)
         return -1;
 
     void **new_ptr = (void **)malloc(sizeof(void *) * new_size);
     if(!new_ptr)
         return -1;
     // 将原本nalloc + 1个元素值拷贝到new_ptr指向的位置
-    memcpy(new_ptr, tk_pq->pq, sizeof(void *) * (tk_pq->nalloc + 1));
+    memcpy(new_ptr, sy_pq->pq, sizeof(void *) * (sy_pq->nalloc + 1));
     // 释放旧元素
-    free(tk_pq->pq);
+    free(sy_pq->pq);
     // 重新改写优先队列元素pq指针为new_ptr
-    tk_pq->pq = new_ptr;
-    tk_pq->size = new_size;
+    sy_pq->pq = new_ptr;
+    sy_pq->size = new_size;
     return 0;
 }
 
-int tk_pq_delmin(tk_pq_t *tk_pq){
-    if(tk_pq_is_empty(tk_pq))
+int sy_pq_delmin(sy_pq_t *sy_pq){
+    if(sy_pq_is_empty(sy_pq))
         return 0;
 
-    exch(tk_pq, 1, tk_pq->nalloc);
-    --tk_pq->nalloc;
-    sink(tk_pq, 1);
-    if((tk_pq->nalloc > 0) && (tk_pq->nalloc <= (tk_pq->size - 1)/4)){
-        if(resize(tk_pq, tk_pq->size / 2) < 0)
+    exch(sy_pq, 1, sy_pq->nalloc);
+    --sy_pq->nalloc;
+    sink(sy_pq, 1);
+    if((sy_pq->nalloc > 0) && (sy_pq->nalloc <= (sy_pq->size - 1)/4)){
+        if(resize(sy_pq, sy_pq->size / 2) < 0)
             return -1;
     }
     return 0;
 }
 
-int tk_pq_insert(tk_pq_t *tk_pq, void *item){
-    if(tk_pq->nalloc + 1 == tk_pq->size){
-        if(resize(tk_pq, tk_pq->size * 2) < 0){
+int sy_pq_insert(sy_pq_t *sy_pq, void *item){
+    if(sy_pq->nalloc + 1 == sy_pq->size){
+        if(resize(sy_pq, sy_pq->size * 2) < 0){
             return -1;
         }
     }
-    tk_pq->pq[++tk_pq->nalloc] = item;
-    swim(tk_pq, tk_pq->nalloc);
+    sy_pq->pq[++sy_pq->nalloc] = item;
+    swim(sy_pq, sy_pq->nalloc);
     return 0;
 }
 
